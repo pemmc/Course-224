a9ecca117e6eb8cd6404d8a67872ea27e3526b04
Paulo Eduardo Magaldi <pauloeduardomagaldi@hotmail.com>
2020-10-27 18:21:25 -0300

    DELEGATE FUNC

--- Course224/Program.cs
+++ Course224/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 using Course224.Entities;
 
@@ -9,6 +10,7 @@ namespace Course224
     {
         static void Main(string[] args)
         {
+            /* USANDAO O ACTION
             //224. Action (exemplo com ForEach)
 
             List<Products> list = new List<Products>();
@@ -41,11 +43,11 @@ namespace Course224
             //FOREACH para a minha LISTA! -- RECEBE UM ACTION
             list.ForEach(action);
 
-            /* SEM ESTE ACTION2 o resultado era:
+            // SEM ESTE ACTION2 o resultado era:
              
-             --- LISTA DE PRODUTOS COM PREÇOS INCREMENTADOS em 10%---
-                Nome: Tv, | Preço: R$990,00
-             */
+             //--- LISTA DE PRODUTOS COM PREÇOS INCREMENTADOS em 10%---
+             //   Nome: Tv, | Preço: R$990,00
+             
 
             //FOREACH para a minha LISTA! -- RECEBE UM ACTION
             list.ForEach(action2);
@@ -58,11 +60,11 @@ namespace Course224
                              });
            
 
-            /* COM ESTE ACTION2 o resultado era:
-             
-             --- LISTA DE PRODUTOS COM PREÇOS INCREMENTADOS em 10%---
-                Nome: Tv, | Preço: R$495,00
-             */
+            // COM ESTE ACTION2 o resultado era:
+             //
+             //--- LISTA DE PRODUTOS COM PREÇOS INCREMENTADOS em 10%---
+             //   Nome: Tv, | Preço: R$495,00
+             //
 
             Console.WriteLine("--- LISTA DE PRODUTOS COM PREÇOS INCREMENTADOS em 10%---");
             foreach (Products p in list)
@@ -81,9 +83,94 @@ namespace Course224
 
             }
 
-            
+            */
+
+            //USANDO O DELEGATE FUNC
+            //225. Func (exemplo com Select)
+
+
+            List<Products> list = new List<Products>();
+
+            list.Add(new Products("Tv", 900.00));
+            list.Add(new Products("Mouse", 50.00));
+            list.Add(new Products("Tablet", 350.50));
+            list.Add(new Products("HD Case", 80.90));
+
+            Console.WriteLine("--- LISTA DE PRODUTOS ---");
+            foreach (Products p in list)
+            {
+                Console.WriteLine(p);
+
+            }
+
+            Console.WriteLine();
+
+
+            List<string> result = list.Select(NameUpper).ToList();
+
+            foreach (string p in result)
+            {
+                Console.WriteLine(p);
+
+            }
+
+            Console.WriteLine();
+
+            Func<Products, string> func = NameLower;
+
+            List<string> result2 = list.Select(func).ToList();
+
+            foreach (string p in result2)
+            {
+                Console.WriteLine(p);
+
+            }
+
+            Console.WriteLine();
+
+            //Já que a func retorna alguma coisa, não precisa das chaves e do RETURN, pois com as chaves é um comando completo
+            Func<Products, string> func2 = p => p.Name.Length.ToString();
+            //ou com as chaves tem que usar o RETURN
+            //Func<Products, string> func3 = p => { return p.Name.Length.ToString(); }
+            //IN LINE
+            // List<string> result3 = list.Select(p => p.Name.Length.ToString()).ToList();
+
+            List<string> result3 = list.Select(func2).ToList();
+
+            foreach (string p in result3)
+            {
+                Console.WriteLine(p);
+
+            }
+
+            Console.WriteLine("--- MINHA FUNCAO RETORNADO NUMEROS DO SELECT ---");
+
+            //com base acima eu gostaria assim
+
+            List<int> result4 = list.Select(p => p.Name.Length).ToList();
+
+            foreach (int p in result4)
+            {
+                Console.WriteLine(p);
+
+            }
+
+            Console.WriteLine();
         }
 
+        static string NameUpper(Products p)
+        {
+            return p.Name.ToUpper();
+
+        }
+
+        static string NameLower(Products p)
+        {
+            return p.Name.ToLower();
+
+        }
+
+        /* USANDO O ACTION
         static void UpdateAumentaPrice(Products p)
         {
             p.Price += p.Price * 0.1;
@@ -95,6 +182,11 @@ namespace Course224
             p.Price -= p.Price * 0.2;
 
         }
+        */
+
+        //USANDO O FUNC
+
+
     }
 
-}
+    }
