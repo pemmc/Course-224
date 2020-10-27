44fdec31c506ecec0fa6bfbc4174bd941c7d1b1a
Paulo Eduardo Magaldi <pauloeduardomagaldi@hotmail.com>
2020-10-27 15:09:06 -0300

    //224. Action (exemplo com ForEach)

---
+++ Course224/Entities/Products.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course224.Entities
+{
+    public class Products
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Products(string name, double price)
+        {
+            Name = name;
+            Price = price;
+
+        }
+
+        public override string ToString()
+        {
+            return "Nome: "
+                    + Name
+                    + ", Preço: "
+                    + Price.ToString("C", new CultureInfo("pt-BR"));
+        }
+
+    }
+
+
+
+}
---
+++ Course224/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Course224.Entities;
+
+namespace Course224
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //224. Action (exemplo com ForEach)
+
+            List<Products> list = new List<Products>();
+
+            list.Add(new Products("Tv", 900.00));
+            list.Add(new Products("Mouse", 50.00));
+            list.Add(new Products("Tablet", 350.50));
+            list.Add(new Products("HD Case", 80.90));
+
+            //FOREACH para a minha LISTA!
+            list.ForEach(UpdatePrice);
+
+            Console.WriteLine("--- LISTA DE PRODUTOS ATUALIZADOS---");
+            foreach (Products p in list)
+            {
+                Console.WriteLine(p);
+
+            }
+        }
+
+        static void UpdatePrice(Products p)
+        {
+            p.Price += p.Price * 0.1;
+
+        }
+    }
+
+}
